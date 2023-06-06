634aa0c223b7c9d783425beff3e3bb77aa1a1e02
EduardoMoreaes <17eduardo05@gmail.com>
2023-06-06 17:17:08 -0300

    cabo veganismo

--- qlearning.py
+++ qlearning.py
@@ -46,6 +46,37 @@ def play(positions, new_positions, matriz):
     print("qlearning: Jogada feita")
     return board
 
+def is_combo(board, position, name):
+        if position == [None, None]:
+            return False
+        
+        x = position[0]
+        y = position[1]
+
+        for i in range(max(1, x - 1), min(8, x + 2)):
+            for j in range(max(1, y - 1), min(8, y + 2)):
+                if (i + j) % 2 == 0:
+                    if board[i][j] == name:
+                        if not (i+1 > 7 or i-1 < 0 or j+1 > 7 or j-1 < 0):
+
+                            if i > x and j > y:
+                                if board[i + 1][j + 1] == "none":
+                                    return True
+                                
+                            elif i > x and j < y:
+                                if board[i + 1][j - 1] == "none":
+                                    return True
+                                
+                            elif i < x and j > y:
+                                if board[i - 1][j + 1] == "none":
+                                    return True
+                                
+                            elif i < x and j < y:
+                                if board[i - 1][j - 1] == "none":
+                                    return True
+                                
+        return False
+
 def test(board):
     if os.path.exists("./game.csv"):
         matriz = board
@@ -81,12 +112,23 @@ def test(board):
                     if(i[0]+1 < 8 and i[1]+1 < 8):
                         x = i[0]+1
                         y = i[1]+1
-                        if (matriz[x][y].lower() == 'none'):
+                        if (matriz[x][y].lower() == 'none' and matriz[i[0]][i[1]] == "B"):
                             matriz = play([i[0], i[1]], [x, y], matriz)
                             reward = 0.5
                             played = True
+
                             break
+                        
+                        elif (matriz[x][y].lower() == 'w' and ((i[0]+2 < 8 and i[1]+2 < 8) and (matriz[x+1][y+1] == "none"))):
+                            matriz = play([i[0], i[1]], [x+1, y+1], matriz)
+                            matriz[x][y] = 'none'
+                            reward = 0.8
                             
+                            if not is_combo:
+                                played = True
+                            
+                            break
+                        
                         else:
                             reward = 0
                     else:
@@ -102,6 +144,16 @@ def test(board):
                             played = True
 
                             break
+                        elif (matriz[x][y].lower() == 'w' and ((i[0]+2 < 8 and i[1]-2 < 8) and (matriz[x+1][y-1] == "none"))):
+                            matriz = play([i[0], i[1]], [x+1, y-1], matriz)
+                            matriz[x][y] = 'none'
+                            reward = 0.8
+                            
+                            if not is_combo:
+                                played = True
+                            
+                            break
+                        
                         else:
                             reward = 0
                     else:
@@ -117,6 +169,16 @@ def test(board):
                             played = True
 
                             break
+                        elif (matriz[x][y].lower() == 'w' and ((i[0]-2 < 8 and i[1]-2 < 8) and (matriz[x-1][y-1] == "none"))):
+                            matriz = play([i[0], i[1]], [x-1, y-1], matriz)
+                            matriz[x][y] = 'none'
+                            reward = 0.8
+                            
+                            if not is_combo:
+                                played = True
+                            
+                            break
+                        
                         else:
                             reward = 0
                     else:
@@ -126,12 +188,22 @@ def test(board):
                     if(i[0]-1 > 0 and i[1]+1 < 8):
                         x = i[0]-1
                         y = i[1]+1
-                        if (matriz[x][y].lower() == 'none'):
+                        if (matriz[x][y].lower() == 'none' and matriz[i[0]][i[1]] == "B"):
                             matriz = play([i[0], i[1]], [x, y], matriz)
                             reward = 0.5
                             played = True
 
                             break
+                        elif (matriz[x][y].lower() == 'w' and ((i[0]-2 < 8 and i[1]+2 < 8) and (matriz[x-1][y+1] == "none"))):
+                            matriz = play([i[0], i[1]], [x-1, y+1], matriz)
+                            matriz[x][y] = 'none'
+                            reward = 0.8
+                            
+                            if not is_combo:
+                                played = True
+                            
+                            break
+                        
                         else:
                             reward = 0
                     else:
@@ -141,6 +213,9 @@ def test(board):
             reward = 0
             break
 
+        for i in positions:
+            if i[1] == 0:
+                matriz[i[0]][i[1]] = 'B'
         agent.update_q_table(state, action, reward, next_state)
         print(agent.q_table)
     QTable().write(agent.q_table)
